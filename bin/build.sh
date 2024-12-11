fb4967a3a64f0ed5979d07d63b49102e7502979c
ZZ <zhangzhang.si@gmail>
2024-12-11 02:48:41 +0000

    Built site for gh-pages

--- bin/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#
-# Build Docker service image(s)
-
-docker build -t nanodiffusion .
--- bin/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Run unit tests
-
-docker run --runtime nvidia -it \
-	-v $(pwd):/workspace \
-	-v $(pwd)/data/container_cache:/root/.cache \
-	nanodiffusion \
-	python -m pytest -p no:warnings "${@:-tests/}"
